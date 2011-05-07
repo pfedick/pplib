b9f58d805a111d5b5694f1c5421da4db284213ac
pafe <pafe@2988137f-5dd0-4e8d-a682-6e3189c43894>
2011-05-07 07:49:12 +0000

    Pointer-Klasse dokumentiert

--- lib/trunk/src/types/Pointer.cpp
+++ lib/trunk/src/types/Pointer.cpp
@@ -48,65 +48,146 @@ namespace ppl7 {
  * \brief %Pointer-Klasse
  *
  * \desc
- * Diese Klasse kann zur Darstellung eines beliebigen Pointers verwendet werden.
+ * Diese Klasse kann zur Darstellung eines beliebigen %Pointers verwendet werden.
+ * Mittels Konstruktor, Operator "=" oder der Funktion Pointer::set kann der %Pointer
+ * gesetzt werden, mit Pointer::ptr und verschiedenen Operatoren kann er wieder
+ * ausgelesen werden.
  *
  */
 
+/*!\brief Konstruktor der Klasse
+ *
+ * \desc
+ * %Pointer wird mit NULL initialisiert
+ */
 Pointer::Pointer()
 {
 	type=POINTER;
 	myptr=NULL;
 }
 
+/*!\brief Copy-Konstruktor
+ *
+ * \desc
+ * %Pointer übernimmt den Wert von \p other.
+ *
+ * @param other Andere %Pointer-Instanz
+ * @return
+ */
 Pointer::Pointer(const Pointer &other)
 {
 	type=POINTER;
 	myptr=other.myptr;
 }
 
+/*!\brief Konstruktor mit Angabe eines %Pointers
+ *
+ * \desc
+ * Mit diesem Konstruktor wird der Wert von \p ptr übernommen.
+ *
+ * @param ptr beliebiger Pointer
+ * @return
+ */
 Pointer::Pointer(void *ptr)
 {
 	type=POINTER;
 	myptr=ptr;
 }
 
+/*!\brief Ist %Pointer Null?
+ *
+ * \desc
+ * Diese Funktion liefert \c true zurück, wenn der Pointer gegenwärtig auf NULL zeigt.
+ * @return Gibt \c true oder \c false zurück.
+ */
 bool Pointer::isNull() const
 {
 	if (myptr==NULL) return true;
 	return false;
 }
 
+/*!\brief %Pointer als "const void *" auslesen
+ *
+ * \desc
+ * Mit dieser Fuktion wird der %Pointer ausgelesen
+ *
+ * \return %Pointer als "const void *"
+ */
 const void *Pointer::ptr() const
 {
 	return myptr;
 }
 
+/*!\brief %Pointer als "const void *" auslesen
+ *
+ * \desc
+ * Mit diesem Operator wird der %Pointer ausgelesen
+ *
+ * \return %Pointer als "const void *"
+ */
 Pointer::operator const void*() const
 {
 	return (const void*)myptr;
 }
 
+/*!\brief %Pointer als "const char *" auslesen
+ *
+ * \desc
+ * Mit diesem Operator wird der %Pointer ausgelesen
+ *
+ * \return %Pointer als "const char *"
+ */
 Pointer::operator const char*() const
 {
 	return (const char*)myptr;
 }
 
+/*!\brief %Pointer als "const unsigned char *" auslesen
+ *
+ * \desc
+ * Mit diesem Operator wird der %Pointer ausgelesen
+ *
+ * \return %Pointer als "const unsigned char *"
+ */
 Pointer::operator const unsigned char*() const
 {
 	return (const unsigned char*)myptr;
 }
 
+/*!\brief %Pointer setzen
+ *
+ * \desc
+ * Mit dieser Funktion wird der Wert des %Pointers gesetzt.
+ *
+ * @param ptr beliebiger %Pointer
+ */
 void Pointer::set(const void *ptr)
 {
 	myptr=(void*)ptr;
 }
 
+/*!\brief %Pointer kopieren
+ *
+ * \desc
+ * Mit diesem Operator wird der Wert des %Pointers \p other übernommen.
+ *
+ * @param other Andere Instanz von %Pointer
+ * @return Liefert eine Referenz auf die Klasse selbst zurück
+ */
 Pointer &Pointer::operator=(const Pointer &other)
 {
 	myptr=other.myptr;
 	return *this;
 }
 
+/*!\brief %Pointer zuweisen
+ *
+ * \desc
+ * Ein beliebiger C-Pointer wird der Klasse zugewiesen.
+ *
+ * @param ptr beliebiger %Pointer
+ * @return Liefert eine Referenz auf die Klasse selbst zurück
+ */
 Pointer &Pointer::operator=(const void *ptr)
 {
 	myptr=(void*)ptr;
