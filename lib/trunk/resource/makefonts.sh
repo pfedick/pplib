c0801513396499fab6bb0a5833fc2008fb2f4530
pafe <pafe@2988137f-5dd0-4e8d-a682-6e3189c43894>
2012-02-06 18:30:56 +0000

    Eingebettete Default-Fonts durch Version 6-Font ersetzt

--- lib/trunk/resource/makefonts.sh
+++ lib/trunk/resource/makefonts.sh
@@ -1,45 +1,69 @@
 #!/bin/sh
+SYSTEM=`uname -s`
+FORMAT=-6
+SUFFIX=fnt6
+ANTIALIASING=--aa4
 
-ANTIALIASING=--aa2
+XORG_FONTPATH=/usr/local/lib/X11/fonts
 
 
 genfontfile() {
 	rm -f $FILE
-	font5maker -t $FILE -q $FONTPATH/$REGULAR -s $SIZE --mono1 -n $NAME --zlib
-	font5maker -t $FILE -q $FONTPATH/$REGULAR -s $SIZE $ANTIALIASING -n $NAME --zlib
-	font5maker -t $FILE -q $FONTPATH/$BOLD -s $SIZE --mono1 --isbold -n $NAME --zlib
-	font5maker -t $FILE -q $FONTPATH/$BOLD -s $SIZE $ANTIALIASING --isbold -n $NAME --zlib
-	font5maker -t $FILE -q $FONTPATH/$ITALIC -s $SIZE --mono1 --isitalic -n $NAME --zlib
-	font5maker -t $FILE -q $FONTPATH/$ITALIC -s $SIZE $ANTIALIASING --isitalic -n $NAME --zlib
-	font5maker -t $FILE -q $FONTPATH/$BOLDITALIC -s $SIZE --mono1 --isbold --isitalic -n $NAME --zlib
-	font5maker -t $FILE -q $FONTPATH/$BOLDITALIC -s $SIZE $ANTIALIASING --isbold --isitalic -n $NAME --zlib
+	pplfontmaker -t $FILE -q $FONTPATH/$REGULAR -s $SIZE --mono1 -n $NAME --zlib $FORMAT
+	if [ $? -ne 0 ] ;then exit; fi
+	pplfontmaker -t $FILE -q $FONTPATH/$REGULAR -s $SIZE $ANTIALIASING -n $NAME --zlib $FORMAT
+	if [ $? -ne 0 ] ;then exit; fi
+	pplfontmaker -t $FILE -q $FONTPATH/$BOLD -s $SIZE --mono1 --isbold -n $NAME --zlib $FORMAT
+	if [ $? -ne 0 ] ;then exit; fi
+	pplfontmaker -t $FILE -q $FONTPATH/$BOLD -s $SIZE $ANTIALIASING --isbold -n $NAME --zlib $FORMAT
+	if [ $? -ne 0 ] ;then exit; fi
+	pplfontmaker -t $FILE -q $FONTPATH/$ITALIC -s $SIZE --mono1 --isitalic -n $NAME --zlib $FORMAT
+	if [ $? -ne 0 ] ;then exit; fi
+	pplfontmaker -t $FILE -q $FONTPATH/$ITALIC -s $SIZE $ANTIALIASING --isitalic -n $NAME --zlib $FORMAT
+	if [ $? -ne 0 ] ;then exit; fi
+	pplfontmaker -t $FILE -q $FONTPATH/$BOLDITALIC -s $SIZE --mono1 --isbold --isitalic -n $NAME --zlib $FORMAT
+	if [ $? -ne 0 ] ;then exit; fi
+	pplfontmaker -t $FILE -q $FONTPATH/$BOLDITALIC -s $SIZE $ANTIALIASING --isbold --isitalic -n $NAME --zlib $FORMAT
+	if [ $? -ne 0 ] ;then exit; fi
 }
 
+genmonospace() {
+	rm -f $FILE
+	pplfontmaker -t $FILE -q $FONTPATH/$REGULAR -s $SIZE --mono1 -n $NAME --zlib $FORMAT
+	if [ $? -ne 0 ] ;then exit; fi
+	pplfontmaker -t $FILE -q $FONTPATH/$REGULAR -s $SIZE $ANTIALIASING -n $NAME --zlib $FORMAT
+	if [ $? -ne 0 ] ;then exit; fi
+	pplfontmaker -t $FILE -q $FONTPATH/$BOLD -s $SIZE --mono1 --isbold -n $NAME --zlib $FORMAT
+	if [ $? -ne 0 ] ;then exit; fi
+	pplfontmaker -t $FILE -q $FONTPATH/$BOLD -s $SIZE $ANTIALIASING --isbold -n $NAME --zlib $FORMAT
+	if [ $? -ne 0 ] ;then exit; fi
+}
 
+###################################################################################################
+# Liberation Sans
 SIZE="8,10,12,14,16,18,20"
 FONTPATH=/usr/share/fonts/truetype/ttf-liberation
+if [ "$SYSTEM" = "FreeBSD" ] ; then FONTPATH="/usr/local/lib/X11/fonts/Liberation"; fi
 REGULAR=LiberationSans-Regular.ttf
 BOLD=LiberationSans-Bold.ttf
 ITALIC=LiberationSans-Italic.ttf
 BOLDITALIC=LiberationSans-BoldItalic.ttf
-NAME="PPL Liberation Sans"
-FILE=liberationsans2.fnt5
-
+NAME="Liberation Sans"
+FILE=liberationsans2.$SUFFIX
+ANTIALIASING=--aa2
 genfontfile
 
+# Liberation Mono
 # Monospace-Fonts, ohne Italic
 SIZE="8,10,12,14"
 FONTPATH=/usr/share/fonts/truetype/ttf-liberation
+if [ "$SYSTEM" = "FreeBSD" ] ; then FONTPATH="/usr/local/lib/X11/fonts/Liberation"; fi
 REGULAR=LiberationMono-Regular.ttf
 BOLD=LiberationMono-Bold.ttf
 ITALIC=LiberationMono-Italic.ttf
 BOLDITALIC=LiberationMono-BoldItalic.ttf
-NAME="PPL Liberation Mono"
-FILE=liberationmono2.fnt5
-
-font5maker -t $FILE -q $FONTPATH/$REGULAR -s $SIZE --mono1 -n $NAME --zlib
-font5maker -t $FILE -q $FONTPATH/$REGULAR -s $SIZE --aa2 -n $NAME --zlib
-font5maker -t $FILE -q $FONTPATH/$BOLD -s $SIZE --mono1 --isbold -n $NAME --zlib
-font5maker -t $FILE -q $FONTPATH/$BOLD -s $SIZE --aa2 --isbold -n $NAME --zlib
-
+NAME="Liberation Mono"
+FILE=liberationmono2.$SUFFIX
+ANTIALIASING=--aa2
+genmonospace
 
