9d05dc1a18fc14a82de96c943bf82aeadaa2c3e4
pafe <pafe@2988137f-5dd0-4e8d-a682-6e3189c43894>
2013-03-21 08:06:18 +0000

    Release 6.4.14 vorbereitet

--- lib/branches/RELENG_6/include/ppl6.h
+++ lib/branches/RELENG_6/include/ppl6.h
@@ -41,7 +41,7 @@
 #define PPL_VERSION_MAJOR	6
 #define PPL_VERSION_MINOR	4
 #define PPL_VERSION_BUILD	14
-#define PPL_RELEASEDATE		20121013
+#define PPL_RELEASEDATE		20130321
 
 // Inlcude PPL6 configuration file
 #ifndef _PPL6_CONFIG
