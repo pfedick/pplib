aa462e9fdfdf26d7edf1e85362b33e3981220a0e
pafe <pafe@2988137f-5dd0-4e8d-a682-6e3189c43894>
2013-04-30 17:14:52 +0000

    Tests für Syslog-Funktionalität

---
+++ lib/branches/RELENG_6/tests/ppl6-tests.h
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * This file is part of "Patrick's Programming Library", Version 7 (PPL7).
+ * Web: http://www.pfp.de/ppl/
+ *
+ * $Author: pafe $
+ * $Revision: 599 $
+ * $Date: 2013-04-26 21:33:45 +0200 (Fr, 26 Apr 2013) $
+ * $Id: ppl7-tests.h 599 2013-04-26 19:33:45Z pafe $
+ *
+ *******************************************************************************
+ * Copyright (c) 2013, Patrick Fedick <patrick@pfp.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *    1. Redistributions of source code must retain the above copyright notice, this
+ *       list of conditions and the following disclaimer.
+ *    2. Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER AND CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
+
+#ifndef PPL6_TESTS_H_
+#define PPL6_TESTS_H_
+
+#ifdef WIN32
+#define DEFAULT_LOCALE	"german"
+#else
+#define DEFAULT_LOCALE	"de_DE.UTF-8"
+#endif
+
+
+#endif /* PPL6_TESTS_H_ */
