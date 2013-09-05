0a35fc327ae134397cd67ec7add1b0d71f2441e6
pafe <pafe@2988137f-5dd0-4e8d-a682-6e3189c43894>
2013-09-05 13:47:19 +0000

    test für MkTime

---
+++ lib/branches/RELENG_6/tests/src/time.cpp
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * This file is part of "Patrick's Programming Library", Version 7 (PPL7).
+ * Web: http://www.pfp.de/ppl/
+ *
+ * $Author: pafe $
+ * $Revision: 668 $
+ * $Date: 2013-05-24 21:56:04 +0200 (Fr, 24 Mai 2013) $
+ * $Id: datetime.cpp 668 2013-05-24 19:56:04Z pafe $
+ * $URL: https://svn.code.sf.net/p/pplib/code/lib/branches/RELENG_6/tests/src/datetime.cpp $
+ *
+ *******************************************************************************
+ * Copyright (c) 2013, Patrick Fedick <patrick@pfp.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER AND CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <locale.h>
+#include <ppl6.h>
+#include <gtest/gtest.h>
+
+namespace {
+
+// The fixture for testing class Foo.
+class TimeTest : public ::testing::Test {
+	protected:
+	TimeTest() {
+		if (setlocale(LC_CTYPE,"de_DE.UTF-8")==NULL) {
+			printf ("setlocale fehlgeschlagen\n");
+			throw std::exception();
+		}
+		ppl6::SetGlobalEncoding("UTF-8");
+	}
+	virtual ~TimeTest() {
+
+	}
+};
+
+TEST_F(TimeTest, MkTime_withCharPtr) {
+	const char *msgtime="2013-09-05T14:43:44+00:00";
+	ASSERT_EQ((ppluint64)1378388624,ppl6::MkTime(msgtime)) << "MkTime returns unexpected value";
+}
+
+
+
+
+
+}
+
+
