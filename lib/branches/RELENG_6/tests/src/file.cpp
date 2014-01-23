faaa4720d48c3d784ddb8b0e814a02aa8c9a6afe
pafe <pafe@2988137f-5dd0-4e8d-a682-6e3189c43894>
2014-01-23 07:33:13 +0000

    Tests erweitert

---
+++ lib/branches/RELENG_6/tests/src/file.cpp
@@ -0,0 +1,95 @@
+/*******************************************************************************
+ * This file is part of "Patrick's Programming Library", Version 6 (PPL6).
+ * Web: http://www.pfp.de/ppl/
+ *
+ * $Author: pafe $
+ * $Revision: 790 $
+ * $Date: 2013-10-21 15:50:51 +0200 (Mo, 21 Okt 2013) $
+ * $Id: CApplication.cpp 790 2013-10-21 13:50:51Z pafe $
+ *
+ *******************************************************************************
+ * Copyright (c) 2014, Patrick Fedick <patrick@pfp.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER AND CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
+
+#include "../include/prolog.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <locale.h>
+#include "../include/ppl6.h"
+#include <gtest/gtest.h>
+#include "ppl6-tests.h"
+
+namespace {
+
+class CFileTest : public ::testing::Test {
+	protected:
+	CFileTest() {
+		if (setlocale(LC_CTYPE,DEFAULT_LOCALE)==NULL) {
+			printf ("setlocale fehlgeschlagen\n");
+			throw std::exception();
+		}
+		ppl6::SetGlobalEncoding("UTF-8");
+	}
+	virtual ~CFileTest() {
+
+	}
+};
+
+TEST_F(CFileTest, ConstructorSimple) {
+	ASSERT_NO_THROW({
+		ppl6::CFile f1;
+		ASSERT_FALSE(f1.IsOpen()) << "File seems to be open, but it shouldn't";
+	}
+	);
+}
+
+TEST_F(CFileTest, openNonexisting) {
+	ppl6::CFile f1;
+	ASSERT_EQ(0,f1.Open("nonexisting.txt"));
+}
+
+TEST_F(CFileTest, openNonexistingUtf8) {
+	ppl6::CFile f1;
+	ASSERT_EQ(0,f1.Open("noneäxisting.txt"));
+}
+
+TEST_F(CFileTest, openExisting) {
+	ppl6::CFile f1;
+	ASSERT_EQ(1,f1.Open("testdata/filenameUSASCII.txt"));
+}
+
+TEST_F(CFileTest, openExistingUtf8) {
+	ppl6::CFile f1;
+	ASSERT_EQ(1,f1.Open("testdata/filenameUTF8äöü.txt"));
+}
+
+
+}
+
+
---
+++ lib/branches/RELENG_6/tests/src/filestatic.cpp
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * This file is part of "Patrick's Programming Library", Version 6 (PPL6).
+ * Web: http://www.pfp.de/ppl/
+ *
+ * $Author: pafe $
+ * $Revision: 790 $
+ * $Date: 2013-10-21 15:50:51 +0200 (Mo, 21 Okt 2013) $
+ * $Id: CApplication.cpp 790 2013-10-21 13:50:51Z pafe $
+ *
+ *******************************************************************************
+ * Copyright (c) 2014, Patrick Fedick <patrick@pfp.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER AND CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
+
+#include "../include/prolog.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <locale.h>
+#include "../include/ppl6.h"
+#include <gtest/gtest.h>
+#include "ppl6-tests.h"
+
+namespace {
+
+class CFileStaticTest : public ::testing::Test {
+	protected:
+	CFileStaticTest() {
+		if (setlocale(LC_CTYPE,DEFAULT_LOCALE)==NULL) {
+			printf ("setlocale fehlgeschlagen\n");
+			throw std::exception();
+		}
+		ppl6::SetGlobalEncoding("UTF-8");
+	}
+	virtual ~CFileStaticTest() {
+
+	}
+};
+
+
+TEST_F(CFileStaticTest, stat) {
+	ppl6::CDirEntry d;
+	ASSERT_NO_THROW({
+		ppl6::CFile::Stat("testdata/filenameUSASCII.txt",d);
+	});
+	ASSERT_EQ((size_t)1962,d.Size);
+	ASSERT_EQ(ppl6::CString("testdata/filenameUSASCII.txt"),d.File);
+}
+
+
+}
+
+
