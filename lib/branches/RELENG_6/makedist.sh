5a02d331826c6026360d682207c4f1d15a04898f
pafe <pafe@2988137f-5dd0-4e8d-a682-6e3189c43894>
2014-05-27 10:20:28 +0000

    Paketname ist jetzt "ppl6" statt "ppl"

--- lib/branches/RELENG_6/makedist.sh
+++ lib/branches/RELENG_6/makedist.sh
@@ -3,31 +3,32 @@
 ##################################################################
 VERSION=$1
 VERSION=${VERSION:=HEAD}
+NAME=ppl6
 
 echo "INFO: create artefakt: dist/ppl-$VERSION.tar.bz2"
 rm -rf dist tmp
-mkdir -p dist tmp/ppl-$VERSION
+mkdir -p dist tmp/$NAME-$VERSION
 if [ $? -ne 0 ] ; then
-    echo "ERROR: could not create directory \"dist\" or \"tmp/ppl-$VERSION\" "
+    echo "ERROR: could not create directory \"dist\" or \"tmp/$NAME-$VERSION\" "
     exit 1
 fi
 
 ##################################################################
 # Library
 find documentation include src *.m4 docs Doxyfile HISTORY.TXT README.TXT configure ppl6-config.in Makefile.in \
-    | grep -v ".svn" | cpio -pdmv tmp/ppl-$VERSION > /dev/null 2>&1
+    | grep -v ".svn" | cpio -pdmv tmp/$NAME-$VERSION > /dev/null 2>&1
 
 if [ $? -ne 0 ] ; then
     echo "ERROR: filecopy failed "
     exit 1
 fi
-rm -rf tmp/ppl-$VERSION/include/*config.h
+rm -rf tmp/$NAME-$VERSION/include/*config.h
    
 ##################################################################
 # Tests
 cd tests
 find  src testdata gcovr Makefile.in ppl6-tests.h \
-    | grep -v ".svn" | cpio -pdmv ../tmp/ppl-$VERSION > /dev/null 2>&1
+    | grep -v ".svn" | cpio -pdmv ../tmp/$NAME-$VERSION > /dev/null 2>&1
 
 if [ $? -ne 0 ] ; then
     echo "ERROR: filecopy failed "
@@ -37,7 +38,7 @@ fi
 ##################################################################
 # TAR-File bauen
 cd ../tmp
-tar -cjf ../dist/ppl-$VERSION.tar.bz2 ppl-$VERSION
+tar -cjf ../dist/$NAME-$VERSION.tar.bz2 $NAME-$VERSION
 if [ $? -ne 0 ] ; then
     echo "ERROR: failed to create tar-file"
     exit 1
