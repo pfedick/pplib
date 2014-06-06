a3a42eb248ce81174e4055d0f9d5ee9c2133d355
pafe <pafe@2988137f-5dd0-4e8d-a682-6e3189c43894>
2014-06-06 11:42:49 +0000

    makedist.sh erstellt

---
+++ lib/trunk/makedist.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# Artefakt bauen
+##################################################################
+VERSION=$1
+VERSION=${VERSION:=HEAD}
+NAME=ppl7
+
+echo "INFO: create artefakt: dist/$NAME-$VERSION.tar.bz2"
+rm -rf dist tmp
+mkdir -p dist tmp/$NAME-$VERSION tmp/$NAME-$VERSION/tests
+if [ $? -ne 0 ] ; then
+    echo "ERROR: could not create directory \"dist\" or \"tmp/$NAME-$VERSION\" "
+    exit 1
+fi
+
+##################################################################
+# Library
+find documentation include src *.m4 docs Doxyfile LICENSE.TXT HISTORY.TXT README.TXT configure ppl7-config.in Makefile.in \
+    | grep -v ".svn" | cpio -pdmv tmp/$NAME-$VERSION > /dev/null 2>&1
+
+if [ $? -ne 0 ] ; then
+    echo "ERROR: filecopy failed "
+    exit 1
+fi
+rm -rf tmp/$NAME-$VERSION/include/*config.h
+   
+##################################################################
+# Tests
+cd tests
+find  src testdata gcovr Makefile.in ppl7-tests.h \
+    | grep -v ".svn" | cpio -pdmv ../tmp/$NAME-$VERSION/tests > /dev/null 2>&1
+
+if [ $? -ne 0 ] ; then
+    echo "ERROR: filecopy failed "
+    exit 1
+fi
+
+##################################################################
+# TAR-File bauen
+cd ../tmp
+tar -cjf ../dist/$NAME-$VERSION.tar.bz2 $NAME-$VERSION
+if [ $? -ne 0 ] ; then
+    echo "ERROR: failed to create tar-file"
+    exit 1
+fi
+echo "INFO: artefakt done"
+exit 0
