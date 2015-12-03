60ed2d737916580e9dd4ff9f2f9730967acf86e2
pfedick <patrick@pfp.de>
2015-12-03 19:10:09 +0100

    fixed memory leak in ByteArray::prepend

--- src/types/ByteArray.cpp
+++ src/types/ByteArray.cpp
@@ -404,6 +404,7 @@ void *ByteArray::prepend(void *adr, size_t size)
 		::free(ptradr);
 		ptradr=NULL;
 		ptrsize=0;
+		free(p);
 		throw Exception();
 	}
 	void *target=(char*)p+size;
@@ -411,6 +412,7 @@ void *ByteArray::prepend(void *adr, size_t size)
 		::free(ptradr);
 		ptradr=NULL;
 		ptrsize=0;
+		free(p);
 		throw Exception();
 	}
 	::free(ptradr);
