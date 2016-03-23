28b2fdd6e252604a2bf89c95e3a90469a40f1d12
pfedick <patrick@pfp.de>
2016-03-23 15:22:10 +0100

    added unittests for class ByteArrayPtr

--- tests/src/bytearrayptr.cpp
+++ tests/src/bytearrayptr.cpp
@@ -121,6 +121,148 @@ TEST_F(ByteArrayPtrTest, ConstructorFromWideString) {
 	);
 }
 
+//0d,0e,37,42,81,ff,42,00,4c,17,12
+static unsigned char binarydata[]={13,14,55,66,129,255,66,0,76,23,18};
+
+TEST_F(ByteArrayPtrTest, isNull) {
+	ppl7::ByteArrayPtr b2;
+	ASSERT_TRUE(b2.isNull());
+	b2.use(binarydata,sizeof(binarydata));
+	ASSERT_FALSE(b2.isNull());
+	b2.use(binarydata,0);
+	ASSERT_FALSE(b2.isNull());
+}
+TEST_F(ByteArrayPtrTest, isEmpty) {
+	ppl7::ByteArrayPtr b2;
+	ASSERT_TRUE(b2.isEmpty());
+	b2.use(binarydata,sizeof(binarydata));
+	ASSERT_FALSE(b2.isEmpty());
+	b2.use(binarydata,0);
+	ASSERT_TRUE(b2.isEmpty());
+}
+
+TEST_F(ByteArrayPtrTest, size) {
+	ppl7::ByteArrayPtr b2(binarydata,sizeof(binarydata));
+	ASSERT_EQ(sizeof(binarydata),b2.size());
+	ppl7::ByteArrayPtr b1;
+	ASSERT_EQ((size_t)0,b1.size());
+}
+
+TEST_F(ByteArrayPtrTest, ptr) {
+	ppl7::ByteArrayPtr b2(binarydata,sizeof(binarydata));
+	ASSERT_EQ((const void*)&binarydata,b2.ptr());
+	ppl7::ByteArrayPtr b1;
+	ASSERT_EQ((const void*)NULL,b1.ptr());
+}
+
+TEST_F(ByteArrayPtrTest, adr) {
+	ppl7::ByteArrayPtr b2(binarydata,sizeof(binarydata));
+	ASSERT_EQ((const void*)&binarydata,b2.adr());
+	ppl7::ByteArrayPtr b1;
+	ASSERT_EQ((const void*)NULL,b1.adr());
+}
+
+TEST_F(ByteArrayPtrTest, toCharPtr) {
+	ppl7::ByteArrayPtr b2(binarydata,sizeof(binarydata));
+	ASSERT_EQ((const char*)&binarydata,b2.toCharPtr());
+	ppl7::ByteArrayPtr b1;
+	ASSERT_EQ((const char*)NULL,b1.toCharPtr());
+}
+
+TEST_F(ByteArrayPtrTest, operatorVoidPtr) {
+	ppl7::ByteArrayPtr b2(binarydata,sizeof(binarydata));
+	ASSERT_EQ((const void*)&binarydata,(const void*)b2);
+}
+
+TEST_F(ByteArrayPtrTest, operatorCharPtr) {
+	ppl7::ByteArrayPtr b2(binarydata,sizeof(binarydata));
+	ASSERT_EQ((const char*)&binarydata,(const char*)b2);
+}
+
+TEST_F(ByteArrayPtrTest, operatorUnsignedCharPtr) {
+	ppl7::ByteArrayPtr b2(binarydata,sizeof(binarydata));
+	ASSERT_EQ((const unsigned char*)&binarydata,(const unsigned char*)b2);
+}
+
+TEST_F(ByteArrayPtrTest, operatorCopy) {
+	ppl7::ByteArrayPtr b1(binarydata,sizeof(binarydata));
+	ppl7::ByteArrayPtr b2;
+	b2=b1;
+	ASSERT_EQ((const void*)&binarydata,b2.adr());
+	ASSERT_EQ(sizeof(binarydata),b2.size());
+}
+
+TEST_F(ByteArrayPtrTest, usePtrAndSize) {
+	ppl7::ByteArrayPtr b2;
+	b2.use(binarydata,sizeof(binarydata));
+	ASSERT_EQ((const void*)&binarydata,b2.adr());
+	ASSERT_EQ(sizeof(binarydata),b2.size());
+}
+
+TEST_F(ByteArrayPtrTest, useOtherByteArrayPtr) {
+	ppl7::ByteArrayPtr b1(binarydata,sizeof(binarydata));
+	ppl7::ByteArrayPtr b2;
+	b2.use(b1);
+	ASSERT_EQ((const void*)&binarydata,b2.adr());
+	ASSERT_EQ(sizeof(binarydata),b2.size());
+}
+
+
+TEST_F(ByteArrayPtrTest, Get) {
+	ppl7::ByteArrayPtr b2(binarydata,sizeof(binarydata));
+	EXPECT_EQ(13,b2.get(0));
+	EXPECT_EQ(14,b2.get(1));
+	EXPECT_EQ(55,b2.get(2));
+	EXPECT_EQ(0,b2.get(7));
+	EXPECT_EQ(23,b2.get(9));
+	EXPECT_EQ(18,b2.get(10));
+
+	ASSERT_THROW(b2.get(11),ppl7::OutOfBoundsEception);
+}
+
+TEST_F(ByteArrayPtrTest, OperatorGet) {
+	ppl7::ByteArrayPtr b2(binarydata,sizeof(binarydata));
+	EXPECT_EQ(13,b2[0]);
+	EXPECT_EQ(14,b2[1]);
+	EXPECT_EQ(55,b2[2]);
+	EXPECT_EQ(0,b2[7]);
+	EXPECT_EQ(23,b2[9]);
+	EXPECT_EQ(18,b2[10]);
+	ASSERT_THROW(b2[11],ppl7::OutOfBoundsEception);
+}
+
+TEST_F(ByteArrayPtrTest, Set) {
+	unsigned char localdata[]={13,14,55,66,129,255,66,0,76,23,18};
+	ppl7::ByteArrayPtr b2(localdata,sizeof(localdata));
+	EXPECT_EQ(13,b2[0]);
+	b2.set(0,69);
+	EXPECT_EQ(69,b2[0]);
+	b2.set(10,123);
+	EXPECT_EQ(123,b2[10]);
+	ASSERT_THROW(b2.set(11,42),ppl7::OutOfBoundsEception);
+}
+
+TEST_F(ByteArrayPtrTest, toHex) {
+	ppl7::ByteArrayPtr b2(binarydata,sizeof(binarydata));
+	ASSERT_EQ(ppl7::String("0d0e374281ff42004c1712"),b2.toHex());
+}
+
+TEST_F(ByteArrayPtrTest, toBase64) {
+	////0d,0e,37,42,81,ff,42,00,4c,17,12
+	ppl7::ByteArrayPtr b2;
+	b2.use(binarydata,7);
+	EXPECT_EQ(ppl7::String("DQ43QoH/Qo=="),b2.toBase64());
+	b2.use(binarydata,8);
+	EXPECT_EQ(ppl7::String("DQ43QoH/QgD="),b2.toBase64());
+	b2.use(binarydata,9);
+	EXPECT_EQ(ppl7::String("DQ43QoH/QgBM"),b2.toBase64());
+	b2.use(binarydata,10);
+	EXPECT_EQ(ppl7::String("DQ43QoH/QgBMFw=="),b2.toBase64());
+	b2.use(binarydata,11);
+	EXPECT_EQ(ppl7::String("DQ43QoH/QgBMFxJ="),b2.toBase64());
+
+}
+
 
 class ByteArrayPtrCompare : public ::testing::Test {
 	protected:
