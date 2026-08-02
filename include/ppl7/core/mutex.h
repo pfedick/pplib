1a010377463c24c8e3f13af15d33006063286319
Patrick Fedick <patrick@pfp.de>
2026-08-02 15:37:15 +0200

    Mutex und PFPFile refactored

--- include/ppl7/core/mutex.h
+++ include/ppl7/core/mutex.h
@@ -32,20 +32,106 @@
 
 namespace ppl7
 {
-//! \brief Synchronisation von Threads
+/**
+ * @class Mutex
+ * @ingroup PPLGroupThreads
+ * @ingroup PPLGroupThreadsMutex
+ * @brief Low-Level-Interface für die Synchronisation von Threads
+ *
+ * Mutexes stellen ein Low-Level-Interface für die Synchronisation von Threads dar. Mit ihnen
+ * kann verhindert werden, dass mehrere Threads versuchen gleichzeitig auf die gleichen Daten
+ * zuzugreifen.
+ *
+ * Unter der Haube verwendet die Klasse std::mutex und std::condition_variable aus der C++
+ * Standardbibliothek.
+ *
+ */
 class Mutex
 {
 private:
     void* handle;
 
 public:
+    /**
+     * @brief Konstruktor
+     *
+     * Der Konstruktor initialisiert den Mutex. Ist dies nicht möglich, wird eine Exception geworfen.
+     *
+     * @exception OutOfMemoryException Es konnte kein Speicher für den Mutex reserviert werden
+     */
     Mutex();
-    ~Mutex() throw();
+
+    /**
+     * @brief Destructor
+     *
+     * Der Destruktor gibt alle Ressourcen des Mutex wieder frei.
+     */
+    ~Mutex() noexcept;
+
+    /**
+     * @brief Mutex sperren
+     *
+     * Diese Funktion versucht einen Mutex zu sperren. Ist dieser bereits durch einen
+     * anderen Thread blockiert, wird gewartet, bis der Mutex wieder frei wird.
+     *
+     * @exception MutexLockingException Mutex konnte nicht gesperrt werden
+     */
     void lock();
+
+    /**
+     * @brief Mutex entsperren
+     *
+     * Mit dieser Funktion wird ein zuvor mit Mutex::lock gesperrter
+     * Mutex wieder frei gegeben.
+     *
+     * @exception MutexLockingException Mutex konnte nicht entsperrt werden
+     */
     void unlock();
-    bool tryLock() throw();
-    int wait(int milliseconds = 0) throw();
-    int signal() throw();
+
+    /**
+     * @brief Mutex versuchen zu sperren
+     *
+     * Diese Funktion versucht wie CMutex::Lock einen Mutex zu sperren.
+     * Ist dieser allerdings bereits durch einen anderen Thread blockiert,
+     * wird nicht gewartet, bis der Mutex wieder frei wird, sondern es wird
+     * sofort ein Fehler zurückgeliefert.
+     *
+     * @return Konnte der Mutex erfolgreich gesperrt werden, liefert die Funktion
+     * true (1) zurück, im Fehlerfall false (0)
+     */
+    bool tryLock() noexcept;
+
+    /**
+     * @brief Auf Signal warten
+     *
+     * Der aufrufende Thread wird angehalten, bis ein anderer Thread diesem Mutex
+     * ein Signal gibt (siehe CMutex::Signal) oder die optional angegebene Zeit
+     * abgelaufen ist.
+     *
+     * @param milliseconds Optionale Angabe einer Zeit, nach deren Ablauf der Thread
+     * automatisch wieder fortgeführt wird. Default = 0 = unendlich.
+     *
+     * @return Liefert true (1), wenn dem Mutex ein Signal gegeben wurde. Wenn @p microseconds
+     * angegeben wurde, liefert die Funktion nach Ablauf des Timeouts 0 zurück, andernfalls wartet
+     * die Funktion so lange, bis ein Signal gegeben wurde.
+     *
+     * @note
+     * War der Mutex vor Aufruf dieser Funktion bereits gelocked, ist er es nach dem Aufruf
+     * immer noch.
+     *
+     */
+    bool wait(int milliseconds = 0) noexcept;
+
+    /**
+     * @brief Signal senden
+     *
+     * Mit dieser Funktion wird dem Mutex ein Signal gegeben. Hat ein Thread sich selbst
+     * durch Aufruf der Wait-Funktion (siehe CMutex::Wait) angehalten, wird er
+     * nach Aufruf des Signals fortgesetzt.
+     *
+     * @return Bei Erfolg liefert die Funktion true (1) zurück, sonst false (0).
+     */
+    bool signal() noexcept;
 };
 } // namespace ppl7
 
--- src/core/Mutex.cpp
+++ src/core/Mutex.cpp
@@ -1,23 +1,18 @@
 /*******************************************************************************
  * This file is part of "Patrick's Programming Library", Version 7 (PPL7).
- * Web: http://www.pfp.de/ppl/
- *
- * $Author$
- * $Revision$
- * $Date$
- * $Id$
- *
+ * Web: https://github.com/pfedick/pplib
  *******************************************************************************
- * Copyright (c) 2013, Patrick Fedick <patrick@pfp.de>
+ * Copyright (c) 2026, Patrick Fedick <patrick@pfp.de>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *    1. Redistributions of source code must retain the above copyright notice, this
- *       list of conditions and the following disclaimer.
- *    2. Redistributions in binary form must reproduce the above copyright notice,
- *       this list of conditions and the following disclaimer in the documentation
- *       and/or other materials provided with the distribution.
+ *
+ *    1. Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *    2. Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -27,292 +22,114 @@
  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGE.
  *******************************************************************************/
 
-#include "prolog_ppl7.h"
+#include <ppl7/core/mutex.h>
+#include <ppl7/exceptions.h>
 
-#ifdef HAVE_STDIO_H
-#include <stdio.h>
-#endif
-#ifdef HAVE_STDLIB_H
-#include <stdlib.h>
-#endif
-#ifdef HAVE_ERRNO_H
-#include <errno.h>
-#endif
-#ifdef HAVE_LIMITS_H
-#include <limits.h>
-#endif
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
 
-#ifdef HAVE_UNISTD_H
-	#include <unistd.h>
-#endif
-#ifdef HAVE_PTHREADS
-	#include <pthread.h>
-#endif
-
-
-#ifdef HAVE_UNISTD_H
-	#include <unistd.h>
-#endif
-#ifdef HAVE_SYS_TIME_H
-	#include <sys/time.h>
-#endif
-
-
-#ifdef _WIN32
-	#define WIN32_LEAN_AND_MEAN		// Keine MFCs
-
-	// ms-help://MS.VSCC.2003/MS.MSDNQTR.2003FEB.1031/sdkintro/sdkintro/using_the_sdk_headers.htm
-	#ifndef _WIN32_WINNT
-	#   define _WIN32_WINNT 0x500
-	#endif
-	#include <windows.h>
-#endif
-
-
-#include "ppl7.h"
-
-namespace ppl7 {
+namespace ppl7
+{
 
 typedef struct tagMutex
 {
-	#ifdef _WIN32
-		HANDLE	handle;
-		HANDLE	condition;
-	#elif defined HAVE_PTHREADS
-		pthread_mutex_t handle;
-		pthread_cond_t	condition;
-	#else
-		//void *handle;
-	#endif
+    std::mutex mtx;
+    std::condition_variable cv;
+    bool signaled{false};
 } PPLMUTEX;
-
-/*!\class Mutex
- * \ingroup PPLGroupThreads
- * \ingroup PPLGroupThreadsMutex
- *
- * Mutexes stellen ein Low-Level-Interface für die Synchronisation von Threads dar. Mit ihnen
- * kann verhindert werden, dass mehrere Threads versuchen gleichzeitig auf die gleichen Daten
- * zuzugreifen.
- *
- */
 
 Mutex::Mutex()
 {
-	handle=::malloc(sizeof(PPLMUTEX));
-	if (!handle) throw OutOfMemoryException();
-	#ifdef _WIN32
-		PPLMUTEX *h=(PPLMUTEX*)handle;
-		h->handle=CreateMutex(NULL,false,NULL);
-		if (!h->handle)
-			throw MutexException("could not create mutex");
-		h->condition = CreateEvent(
-		        NULL,         // default security attributes
-		        TRUE,         // manual-reset event
-		        TRUE,         // initial state is signaled
-		        NULL 		 // object name
-		        );
-		if (!h->condition)
-			throw MutexException("could not create mutex condition");
-
-	#elif defined HAVE_PTHREADS
-		PPLMUTEX *h=(PPLMUTEX*)handle;
-		pthread_mutexattr_t Attr;
-		pthread_mutexattr_init(&Attr);
-		pthread_mutexattr_settype(&Attr, PTHREAD_MUTEX_ERRORCHECK);
-		pthread_mutex_init(&h->handle,&Attr);
-		pthread_mutexattr_destroy(&Attr);
-		/*
-		pthread_mutex_init(&h->handle,NULL);
-		*/
-		pthread_cond_init(&h->condition,NULL);
-	#else
-		throw UnsupportedFeatureException();
-	#endif
+    handle = new (std::nothrow) PPLMUTEX();
+    if (!handle) throw OutOfMemoryException();
 }
 
-Mutex::~Mutex() throw()
-		{
-	if (handle==0) return;
-#ifdef _WIN32
-	PPLMUTEX *h=(PPLMUTEX*)handle;
-	CloseHandle(h->handle);
-#elif defined HAVE_PTHREADS
-	PPLMUTEX *h=(PPLMUTEX*)handle;
-	pthread_mutex_destroy(&h->handle);
-	int rc=pthread_cond_destroy(&h->condition);
-	if (rc == EBUSY) {	// Thread warten noch auf Condition-Variable
-		pthread_cond_signal(&h->condition);
-	}
-#endif
-	::free(handle);
-	handle=NULL;
-		}
+Mutex::~Mutex() noexcept
+{
+    if (handle) {
+        PPLMUTEX* h = static_cast<PPLMUTEX*>(handle);
+        delete h;
+        handle = nullptr;
+    }
+}
 
-/*!\brief Mutex sperren
- *
- * Diese Funktion versucht einen Mutex zu sperren. Ist dieser bereits durch einen
- * anderen Thread blockiert, wird gewartet, bis der Mutex wieder frei wird.
- *
- * \exception DeadlockException Wird geworfen, wenn durch das Sperren des Mutex ein
- * Deadlock entstehen würde
- * \exception MutexLockingException Mutex konnte nicht gesperrt werden
- */
 void Mutex::lock()
 {
-#ifdef _WIN32
-	PPLMUTEX *h=(PPLMUTEX*)handle;
-	DWORD ret=WaitForSingleObject(h->handle,INFINITE);
-	if (ret==WAIT_OBJECT_0) return;
-    throw MutexLockingException("Mutex::lock");
-#elif defined HAVE_PTHREADS
-	PPLMUTEX *h = (PPLMUTEX*) handle;
-	int ret = pthread_mutex_lock(&h->handle);
-	if (ret == 0) return;
-	else if (ret==EDEADLK) throw DeadlockException("Mutex::lock");
-	throw MutexLockingException("Mutex::lock");
-#endif
+    PPLMUTEX* h = static_cast<PPLMUTEX*>(handle);
+    try {
+        h->mtx.lock();
+    }
+    catch (...) {
+        throw MutexLockingException("Mutex::lock");
+    }
 }
 
-/*!\brief Mutex entsperren
- *
- * Mit dieser Funktion wird ein zuvor mit Mutex::lock gesperrter
- * Mutex wieder frei gegeben.
- *
- * \exception MutexNotLockedException Mutex war nicht gesperrt
- * \exception MutexLockingException Mutex konnte nicht entsperrt werden
- */
 void Mutex::unlock()
 {
-#ifdef _WIN32
-	PPLMUTEX *h=(PPLMUTEX*)handle;
-	if (ReleaseMutex(h->handle)) return;
-	throw MutexLockingException("Mutex::unlock");
-#elif defined HAVE_PTHREADS
-	PPLMUTEX *h=(PPLMUTEX*)handle;
-	int ret = pthread_mutex_unlock(&h->handle);
-	if (ret == 0) return;
-	if (ret == EPERM) throw MutexNotLockedException("Mutex::unlock");
-	throw MutexLockingException("Mutex::unlock");
-#endif
+    PPLMUTEX* h = static_cast<PPLMUTEX*>(handle);
+    try {
+        h->mtx.unlock();
+    }
+    catch (...) {
+        throw MutexLockingException("Mutex::unlock");
+    }
 }
 
-bool Mutex::tryLock() throw ()
-/*!\brief Mutex versuchen zu sperren
- *
- * Diese Funktion versucht wie CMutex::Lock einen Mutex zu sperren.
- * Ist dieser allerdings bereits durch einen anderen Thread blockiert,
- * wird nicht gewartet, bis der Mutex wieder frei wird, sondern es wird
- * sofort ein Fehler zurückgeliefert.
- *
- * \return Konnte der Mutex erfolgreich gesperrt werden, liefert die Funktion
- * true (1) zurück, im Fehlerfall false (0)
- */
+bool Mutex::tryLock() noexcept
 {
-#ifdef _WIN32
-	PPLMUTEX *h=(PPLMUTEX*)handle;
-	DWORD ret=WaitForSingleObject(h->handle,0);
-	if (ret==WAIT_TIMEOUT) return false;
-	if (ret!=WAIT_FAILED) return true;
-	return false;
-#elif defined HAVE_PTHREADS
-	PPLMUTEX *h = (PPLMUTEX*) handle;
-	int ret = pthread_mutex_trylock(&h->handle);
-	if (ret == 0)
-		return true;
-	return false;
-#endif
+    PPLMUTEX* h = static_cast<PPLMUTEX*>(handle);
+    return h->mtx.try_lock();
 }
 
-
-int Mutex::wait(int milliseconds) throw ()
-/*! \brief Auf Signal warten
- *
- * Der aufrufende Thread wird angehalten, bis ein anderer Thread diesem Mutex
- * ein Signal gibt (siehe CMutex::Signal) oder die optional angegebene Zeit
- * abgelaufen ist.
- *
- * \param milliseconds Optionale Angabe einer Zeit, nach deren Ablauf der Thread
- * automatisch wieder fortgeführt wird. Default = 0 = unendlich.
- *
- * \return Liefert true (1), wenn dem Mutex ein Signal gegeben wurde. Wenn \p microseconds
- * angegeben wurde, liefert die Funktion nach Ablauf des Timeouts 0 zurück, andernfalls wartet
- * die Funktion so lange, bis ein Signal gegeben wurde.
- *
- * \note
- * War der Mutex vor Aufruf dieser Funktion bereits gelocked, ist er es nach dem Aufruf
- * immer noch.
- *
- */
+bool Mutex::wait(int milliseconds) noexcept
 {
-#ifdef _WIN32
-	PPLMUTEX *h=(PPLMUTEX*)handle;
-	ResetEvent(h->condition);
-	int ret;
-	if (milliseconds) ret=WaitForSingleObject(h->condition,milliseconds);
-	else ret=WaitForSingleObject(h->condition,INFINITE);
-	if (ret==WAIT_OBJECT_0) return 1;
-	return 0;
-#elif defined HAVE_PTHREADS
-	PPLMUTEX *h = (PPLMUTEX*) handle;
-	int ret = pthread_mutex_trylock(&h->handle);
-	int releaseatend = 0;
-	if (ret == 0) {
-		releaseatend = 1;
-	} else if (ret == EBUSY) { // Mutex ist schon gelocked
-		releaseatend = 0;
-	} else {
-		return 0;
-	}
-	if (milliseconds > 0) {
-		struct timeval now;
-		struct timespec timeout;
-		// Get current Time
-		gettimeofday(&now, NULL);
-		timeout.tv_nsec = now.tv_usec * 1000 + (milliseconds * 1000000);
-		timeout.tv_sec = now.tv_sec + (timeout.tv_nsec / 1000000000);
-		timeout.tv_nsec = timeout.tv_nsec % 1000000000;
-		ret = pthread_cond_timedwait(&h->condition, &h->handle, &timeout);
-	} else {
-		ret = pthread_cond_wait(&h->condition, &h->handle);
-	}
-	if (releaseatend)
-		pthread_mutex_unlock(&h->handle);
-	if (ret == 0) {
-		return 1;
-	}
-	return 0;
-#endif
+    if (!handle) return false;
+    PPLMUTEX* h = static_cast<PPLMUTEX*>(handle);
+    try {
+        std::unique_lock<std::mutex> lock(h->mtx);
+
+        auto predicate = [h]() { return h->signaled; };
+
+        bool success = false;
+        if (milliseconds > 0) {
+            success = h->cv.wait_for(lock, std::chrono::milliseconds(milliseconds), predicate);
+        } else {
+            h->cv.wait(lock, predicate);
+            success = true;
+        }
+
+        if (success) {
+            h->signaled = false; // Event für die nächste Benutzung zurücksetzen
+            return true;
+        }
+        return false;
+    }
+    catch (...) {
+        return false;
+    }
 }
 
-
-int Mutex::signal() throw ()
-/*! \brief Signal senden
- *
- * Mit dieser Funktion wird dem Mutex ein Signal gegeben. Hat ein Thread sich selbst
- * durch Aufruf der Wait-Funktion (siehe CMutex::Wait) angehalten, wird er
- * nach Aufruf des Signals fortgesetzt.
- *
- * \return Bei Erfolg liefert die Funktion true (1) zurück, sonst false (0).
- */
+bool Mutex::signal() noexcept
 {
-#ifdef _WIN32
-	PPLMUTEX *h=(PPLMUTEX*)handle;
-	int ret=SetEvent(h->condition);
-	if (ret==0) return 0;
-	return 1;
-#elif defined HAVE_PTHREADS
-	PPLMUTEX *h = (PPLMUTEX*) handle;
-	int ret = pthread_cond_signal(&h->condition);
-	if (ret == 0)
-		return 1;
-	return 0;
-#endif
+    if (!handle) return false;
+    PPLMUTEX* h = static_cast<PPLMUTEX*>(handle);
+    try {
+        {
+            std::lock_guard<std::mutex> lock(h->mtx);
+            h->signaled = true;
+        }
+        h->cv.notify_one();
+        return true;
+    }
+    catch (...) {
+        return false;
+    }
 }
 
-
-} // EOF namespace ppl
+} // namespace ppl7
